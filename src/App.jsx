b7d8acf6f2058618f69aa0a86489f94628b6c6df
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2022-01-31 10:58:06 -0600

    set route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./screens/HomePage/HomePage";
+import Homes from "./screens/Homes/Homes";
+import Lots from "./screens/Lots/Lots";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/homes" element={<Homes />} />
+          <Route path="/lots" element={<Lots />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
