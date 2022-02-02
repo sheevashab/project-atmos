9ba43d68b42ada29a2d00c267187a075e59e549e
Sheeva Shabahang <sheevashabahang@Sheevas-MacBook-Pro.local>
2022-02-02 15:18:01 -0600

    HomePlans cards styled

--- src/screens/HomePlans/HomePlans.jsx
+++ src/screens/HomePlans/HomePlans.jsx
@@ -9,19 +9,21 @@ class HomePlans extends Component {
     const homePlansList = homePlans.length ? (
       homePlans.map((homePlan) => {
         return (
-          <div key={homePlan.homePlanId}>
-            <img
-              src={homePlan.image}
-              alt="house exterior"
-              className="home-image"
-            />
-            <h2>{homePlan.name}</h2>
-            <h3>
-              {homePlan.numBeds} beds - {homePlan.numBaths} baths -{" "}
-              {homePlan.sqft} sqft
-            </h3>
-            <h4>{homePlan.tags}</h4>
-            <p>{homePlan.description}</p>
+          <div key={homePlan.homePlanId} className="home-card">
+            <button>
+              <img
+                src={homePlan.image}
+                alt="house exterior"
+                className="home-image"
+              />
+              <h2>{homePlan.name}</h2>
+              <h3>
+                {homePlan.numBeds} beds - {homePlan.numBaths} baths -{" "}
+                {homePlan.sqft} sqft
+              </h3>
+              <h4 className="home-tags">{homePlan.tags}</h4>
+              <p>{homePlan.description}</p>
+            </button>
           </div>
         );
       })
@@ -31,8 +33,9 @@ class HomePlans extends Component {
 
     return (
       <Layout>
-        <div className="home-list">
-          <div className="home-card">{homePlansList}</div>
+        <button className="button-show">Show Saved Homes</button>
+        <div className="home-container">
+          <div className="home-list">{homePlansList}</div>
         </div>
       </Layout>
     );
